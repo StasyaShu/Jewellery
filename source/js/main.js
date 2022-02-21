24b247606e3521eea936d8d328cb8c2662a2bc50
StasyaShu <stanislava.semenova@gmail.com>
2022-02-21 18:36:19 +0300

    аккордеон 100%

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,5 @@
+// Мобильное бургер-меню
+
 const handleMobileMenu = (() => {
   const siteNavigation = document.querySelector('.page-header');
   const siteNavigationToggle = document.querySelector('.page-header__toggle');
@@ -37,3 +39,47 @@ const handleMobileMenu = (() => {
 
 handleMobileMenu.showNav();
 // handleMobileMenu.clickNavLink();
+
+// Аккордеон на главной странице
+
+const handleAccordion = (() => {
+  const accordion = document.getElementById('accordion');
+
+  const hideAll = () => {
+    const h3El = accordion.querySelectorAll('h3');
+    const divEl = accordion.querySelectorAll('.questions__text');
+
+    for (let i = 0; i < h3El.length; i++) {
+      h3El[i].classList.remove('questions__title--select');
+    }
+    for (let i = 0; i < divEl.length; i++) {
+      divEl[i].style.height = '0';
+    }
+  }
+
+  const showText = (textEl) => {
+    textEl.style.height = textEl.scrollHeight + 'px';
+  }
+
+  return {
+
+    manageAccordion: () => {
+      if (accordion) {
+        accordion.addEventListener('click', (evt) => {
+
+          const targ = evt.target;
+          if (targ.tagName !== 'H3') return;
+
+          if (targ.classList.contains('questions__title--select')) {
+            hideAll();
+          } else {
+            hideAll();
+            targ.classList.add('questions__title--select');
+            showText(targ.nextElementSibling);
+          }
+        })
+      }
+    }
+  }
+})()
+handleAccordion.manageAccordion();
